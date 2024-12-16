68143e07b38cb191cc76c706c2038aacb7c9c26a
emran <nahiangazi31@gmail.com>
2024-12-16 09:01:00 +0600

    code upload

---
+++ index.js
@@ -0,0 +1,70 @@
+
+// for(let i=1; i<=10; i++ ){
+// // console.log(`${i}*5= ${i*5}`);
+
+// // console.log(`${i*5}`);
+// // console.log(i*5);
+// // console.log(i + "*5=" + (i * 5));
+// console.log("hello" + i);
+
+// }
+
+// let students={
+//     Alice:85,
+//     Bob:95,
+//     Charle:80,
+// }
+
+// for(let student in students){
+//  console.log(`${student} has been scored ${students[student]}`);
+// }
+
+// const fruits=["Apple","guava","mango"]
+// for (let fruit of fruits){
+//    console.log(`I like ${fruit}`);
+// }
+
+// const names="EMRAN";
+// for(let name of names ){
+//  console.log(name);
+// }
+
+// const students={
+//     Emran:10,
+//     Nahian:5,
+//     gazi:25,
+// }
+
+// for(let student in students){
+
+//     console.log(`${student} has been scoored ${students[student]}`);
+// }
+
+// const studentMap= new Map([
+//     [101, 'emran'],
+//     [102, 'naif'],
+//     [103, 'saif']
+// ]);
+// studentMap.forEach((name, id)=>{
+//  console.log(`${id} is ${name}`);
+// });
+
+// let countDown=10;
+// // console.log("countdown starts");
+// while(countDown > 0){
+
+//     console.log(`${countDown}`);
+//     countDown--;
+
+// }
+// let i=1;
+// do{
+//  console.log(`count is start ${i}`);
+//  i++;
+// } while(i<=7);
+let num=29;
+let i=1;
+do{
+console.log(`${num} * ${i} = ${num * i}`);
+i++;
+}while(i<=10);
